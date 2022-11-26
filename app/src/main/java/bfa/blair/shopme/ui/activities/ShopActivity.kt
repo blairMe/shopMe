381c5a1a7e6c8597115171ac50bf2cf1fa7491cf
blairMe <felixanachoni@gmail.com>
2022-11-26 22:40:46 +0300

    E-Shop

---
+++ app/src/main/java/bfa/blair/shopme/ui/activities/ShopActivity.kt
@@ -0,0 +1,24 @@
+package bfa.blair.shopme.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import bfa.blair.shopme.R
+import bfa.blair.shopme.databinding.ActivityShopBinding
+
+class ShopActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityShopBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityShopBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.nav_host_fragment_shop_activity)
+        setupActionBarWithNavController(navController, null)
+
+    }
+}
---
+++ app/src/main/java/bfa/blair/shopme/ui/fragments/SplashScreenFragment.kt
@@ -0,0 +1,28 @@
+package bfa.blair.shopme.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import bfa.blair.shopme.R
+import bfa.blair.shopme.databinding.FragmentSplashScreenBinding
+
+class SplashScreenFragment : Fragment() {
+
+    lateinit var binding: FragmentSplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentSplashScreenBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
