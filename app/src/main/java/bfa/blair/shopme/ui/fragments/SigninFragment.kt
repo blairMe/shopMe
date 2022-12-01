e89b7f229a297cd1506f822327898ad24b63fe5a
blairMe <felixanachoni@gmail.com>
2022-12-01 21:24:12 +0300

    E-Shop

--- app/src/main/java/bfa/blair/shopme/ui/fragments/SigninFragment.kt
+++ app/src/main/java/bfa/blair/shopme/ui/fragments/SigninFragment.kt
@@ -1,21 +1,25 @@
 package bfa.blair.shopme.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import bfa.blair.shopme.R
 import bfa.blair.shopme.databinding.FragmentSigninBinding
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
+import kotlinx.coroutines.tasks.await
+import java.io.IOException
 
 class SigninFragment : Fragment() {
 
@@ -32,7 +36,7 @@ class SigninFragment : Fragment() {
     override fun onStart() {
         super.onStart()
         val currentUser = firebaseAuth.currentUser
-        if(currentUser != null){
+        if (currentUser != null) {
             view?.let {
                 Navigation.findNavController(it)
                     .navigate(R.id.action_signinFragment_to_homeFragment)
@@ -55,49 +59,28 @@ class SigninFragment : Fragment() {
 
         firebaseAuth = FirebaseAuth.getInstance()
 
-
         binding.signuPgBtn.setOnClickListener {
             Navigation.findNavController(view)
                 .navigate(R.id.action_signinFragment_to_signupFragment)
         }
 
-        val email = binding.usersEmail.text.toString()
-        val password = binding.usersPassword.text.toString()
-
         binding.signinBtn.setOnClickListener {
 
-            lifecycleScope.launch(Dispatchers.IO) {
+            val email = binding.usersEmail.text.toString()
+            val password = binding.usersPassword.text.toString()
 
+            CoroutineScope(Dispatchers.IO).launch {
                 try {
                     firebaseAuth.signInWithEmailAndPassword(email, password).await()
 
-
-
+                    withContext(Dispatchers.Main) {
+                            findNavController().navigate(R.id.action_signinFragment_to_homeFragment)
+                    }
+                } catch (ex: Exception) {
+                    Log.e("Error", ex.message.toString())
                 }
             }
 
-
-
-//                auth.signInWithEmailAndPassword(email, password)
-//                .addOnCompleteListener(requireActivity()) { task ->
-//                    if (task.isSuccessful) {
-//                        view.let {
-//                            Navigation.findNavController(it)
-//                                .navigate(R.id.action_signupFragment_to_homeFragment)
-//                        }
-//
-//                    }
-//                }
-
         }
-
-
-
-
-
-
-
-
     }
-
 }
--- app/src/main/java/bfa/blair/shopme/ui/fragments/SignupFragment.kt
+++ app/src/main/java/bfa/blair/shopme/ui/fragments/SignupFragment.kt
@@ -6,19 +6,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import bfa.blair.shopme.R
 import bfa.blair.shopme.databinding.FragmentSignupBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.*
+import kotlinx.coroutines.tasks.await
 
 
 class SignupFragment : Fragment() {
 
     lateinit var binding: FragmentSignupBinding
 
-    private var auth: FirebaseAuth = Firebase.auth
+    lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +42,8 @@ class SignupFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+
         binding.signinPgBtn.setOnClickListener {
             Navigation.findNavController(view)
                 .navigate(R.id.action_signupFragment_to_signinFragment)
@@ -44,29 +51,23 @@ class SignupFragment : Fragment() {
 
 
         val userName = binding.tvUsername.text.toString()
-
+        val email = binding.tvEmail.text.toString()
+        val password = binding.tvPassword.text.toString()
 
         binding.signupBtn.setOnClickListener {
 
-            auth = Firebase.auth
+            CoroutineScope(Dispatchers.IO).launch {
 
-            val email = binding.tvEmail.text.toString()
-            val password = binding.tvPassword.text.toString()
+                try {
+                    firebaseAuth.createUserWithEmailAndPassword(email, password).await()
 
-            auth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(requireActivity()) { task ->
-                    if (task.isSuccessful) {
-                        view.let {
-                            Navigation.findNavController(it)
-                                .navigate(R.id.action_signupFragment_to_homeFragment)
-                        }
-
-                    } else {
-                        Log.i("Cannot Signup", "Unable to signup")
+                    withContext(Dispatchers.Main) {
+                            findNavController().navigate(R.id.action_signupFragment_to_homeFragment)
                     }
+                } catch (ex: Exception) {
+                    Log.e("Error", ex.message.toString())
                 }
+            }
         }
     }
-
-
 }
