c174756a06350daa2e0a0e37abba0e25d70e7671
blairMe <felixanachoni@gmail.com>
2022-12-01 07:15:22 +0300

    E-Shop

--- app/src/main/java/bfa/blair/shopme/ui/fragments/SigninFragment.kt
+++ app/src/main/java/bfa/blair/shopme/ui/fragments/SigninFragment.kt
@@ -5,18 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.Navigation
 import bfa.blair.shopme.R
 import bfa.blair.shopme.databinding.FragmentSigninBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class SigninFragment : Fragment() {
 
     lateinit var binding: FragmentSigninBinding
 
-    private var auth: FirebaseAuth = Firebase.auth
+    private lateinit var firebaseAuth: FirebaseAuth
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +31,7 @@ class SigninFragment : Fragment() {
 
     override fun onStart() {
         super.onStart()
-        val currentUser = auth.currentUser
+        val currentUser = firebaseAuth.currentUser
         if(currentUser != null){
             view?.let {
                 Navigation.findNavController(it)
@@ -47,25 +53,41 @@ class SigninFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+
+
         binding.signuPgBtn.setOnClickListener {
             Navigation.findNavController(view)
                 .navigate(R.id.action_signinFragment_to_signupFragment)
         }
 
+        val email = binding.usersEmail.text.toString()
+        val password = binding.usersPassword.text.toString()
+
         binding.signinBtn.setOnClickListener {
-            val email = binding.usersEmail.text.toString()
-            val password = binding.usersPassword.text.toString()
 
-            auth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(requireActivity()) { task ->
-                    if (task.isSuccessful) {
-                        view.let {
-                            Navigation.findNavController(it)
-                                .navigate(R.id.action_signupFragment_to_homeFragment)
-                        }
+            lifecycleScope.launch(Dispatchers.IO) {
 
-                    }
+                try {
+                    firebaseAuth.signInWithEmailAndPassword(email, password).await()
+
+
+
                 }
+            }
+
+
+
+//                auth.signInWithEmailAndPassword(email, password)
+//                .addOnCompleteListener(requireActivity()) { task ->
+//                    if (task.isSuccessful) {
+//                        view.let {
+//                            Navigation.findNavController(it)
+//                                .navigate(R.id.action_signupFragment_to_homeFragment)
+//                        }
+//
+//                    }
+//                }
 
         }
 
