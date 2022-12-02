4d9edae73e2bdd473c0291f7ed71ed29fe7dc97a
blairMe <felixanachoni@gmail.com>
2022-12-02 20:35:42 +0300

    E-Shop

--- app/src/main/java/bfa/blair/shopme/ui/fragments/SignupFragment.kt
+++ app/src/main/java/bfa/blair/shopme/ui/fragments/SignupFragment.kt
@@ -36,10 +36,12 @@ class SignupFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentSignupBinding.inflate(inflater, container, false)
         return binding.root
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
 
         firebaseAuth = FirebaseAuth.getInstance()
 
